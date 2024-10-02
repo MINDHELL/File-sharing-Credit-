cf8ef079128d79290439d74deb0be3582c0c9949
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-02 11:26:23 -0700

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,4 +1,4 @@
-
+from pymongo import MongoClient
 import motor.motor_asyncio
 from config import DB_URI, DB_NAME
 
@@ -14,25 +14,16 @@ default_verify = {
     'link': ""
 }
 
-def new_user(id):
-    return {
-        '_id': id,
-        'verify_status': {
-            'is_verified': False,
-            'verified_time': "",
-            'verify_token': "",
-            'link': ""
-        }
-    }
+client = MongoClient(DB_URI)
+db = client[DB_NAME]
+users = db["users"]
+
+async def add_user(id):
+    users.insert_one({"_id": id, "limit": 10, "is_verified": False, "verify_token": "", "verified_time": 0})
 
 async def present_user(user_id: int):
     found = await user_data.find_one({'_id': user_id})
     return bool(found)
-
-async def add_user(user_id: int):
-    user = new_user(user_id)
-    await user_data.insert_one(user)
-    return
 
 async def db_verify_status(user_id):
     user = await user_data.find_one({'_id': user_id})
@@ -51,3 +42,26 @@ async def full_userbase():
 async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
     return
+
+async def update_user_limit(id, limit):
+    users.update_one({"_id": id}, {"$set": {"limit": limit}})
+
+async def get_user_limit(id):
+    user = users.find_one({"_id": id})
+    return user.get("limit", 0) if user else 0
+
+async def get_verify_status(id):
+    user = users.find_one({"_id": id})
+    return user if user else {"is_verified": False, "verify_token": "", "verified_time": 0}
+
+async def update_verify_status(id, is_verified=None, verify_token=None, verified_time=None):
+    update_fields = {}
+    if is_verified is not None:
+        update_fields["is_verified"] = is_verified
+    if verify_token is not None:
+        update_fields["verify_token"] = verify_token
+    if verified_time is not None:
+        update_fields["verified_time"] = verified_time
+
+    users.update_one({"_id": id}, {"$set": update_fields})
+
