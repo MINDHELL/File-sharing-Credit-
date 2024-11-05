2eedb38b1641dfad8669503ac2a6ea356baf1885
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-05 19:42:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ START_COMMAND_LIMIT = 15  # Default limit for new users
 LIMIT_INCREASE_AMOUNT = 10  # Amount by which the limit is increased after verification
 AUTO_DELETE_DELAY = 600 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7413748827:AAHGW5KP6V8CPIhE3sUJPwdHAUq7MkNKzRo")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7447026397:AAHeIQzJGiOxpW1ud5EbM8pqNOxqUAPx3NI")
 APP_ID = int(os.environ.get("APP_ID", "22505271"))
 API_HASH = os.environ.get("API_HASH", "c89a94fcfda4bc06524d0903977fc81e")
  
